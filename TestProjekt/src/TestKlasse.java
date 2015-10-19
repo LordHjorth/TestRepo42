8b39a98d389caf52d4000543b61a0788292f3457
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-10-19 16:10:20 +0200

    !!!

--- TestProjekt/src/TestKlasse.java
+++ TestProjekt/src/TestKlasse.java
@@ -7,5 +7,6 @@ public class TestKlasse {
     // Jeg elsker bare terminal! :D
     // Folk der ikke bruger git gennem terminalen er tosser! :O
     //Herre meget online rettelse
-	//Get rekt bitches
+	//Get rekt bitches!!!
+	
 }
