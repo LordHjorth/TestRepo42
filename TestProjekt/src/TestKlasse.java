3c62e8151057eeed09d0f782e734dce981caa114
Christian <chrbudtz@gmail.com>
2015-10-19 16:14:02 +0200

    Håber på at det går godt

--- TestProjekt/src/TestKlasse.java
+++ TestProjekt/src/TestKlasse.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 public class TestKlasse {
 	//Interessant tekst!
@@ -10,17 +10,6 @@ public class TestKlasse {
     //Herre meget online rettelse
     // Christian er mega flabet!!! :O
 }
-=======
 
-public class TestKlasse {
-	//Interessant tekst!
-	//Mere interessant tekst!
-	//Anden spændende tekst!
-    // Jeg er fucking awesome
-    // Jeg elsker bare terminal! :D
-    // Folk der ikke bruger git gennem terminalen er tosser! :O
-    //Herre meget online rettelse
-	//Get rekt bitches!!!
-	
-}
->>>>>>> FETCH_HEAD
+
+
