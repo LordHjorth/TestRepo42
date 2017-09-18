a4725aedf25b77c43e3e5e1cf40876c1bdb0e5dd
Christian Budtz <cbudtz@users.noreply.github.com>
2017-09-18 12:30:09 +0200

    Oprydning fra sidste semester

--- TestProjekt/src/TestKlasse.java
+++ TestProjekt/src/TestKlasse.java
@@ -3,15 +3,11 @@
 public class TestKlasse {
 	//Interessant tekst!
 	//Mere interessant tekst!
-	//Anden spændende tekst!
+	//Anden spÃ¦ndende tekst!
     // Jeg er fucking awesome
     // Jeg elsker bare terminal! :D
     // Folk der ikke bruger git gennem terminalen er tosser! :O
-    // Christian er s� flabet! Han burde skamme sig! :P
     //Herre meget online rettelse
-    // Christian er mega flabet!!! :O
-    // Christian er stadig flabet....
-    // Christian er dejlig <3 :D
 }
 
 
